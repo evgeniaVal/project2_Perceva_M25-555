b3bd1aabdbee5a09dacbf6af4eacde1f89246a55
evgeniaVal <perceva221@gmail.com>
2026-01-10 12:30:58 +0300

    add save_metadata()

--- src/primitive_db/utils.py
+++ src/primitive_db/utils.py
@@ -20,3 +20,19 @@ def load_metadata(filepath: str) -> dict:
     except json.JSONDecodeError as e:
         print(f"Ошибка декодирования JSON из файла {filepath}: {e}")
     return {}
+
+def save_metadata(filepath: str, data: dict) -> None:
+    """Сохраняет метаданные в json файл.
+
+    Args:
+        filepath (str): Путь к json файлу для сохранения метаданных.
+        data (dict): Метаданные в виде словаря для сохранения.
+    """
+    try:
+        with open(filepath, 'w') as f:
+            json.dump(data, f, indent=4)
+    except (FileNotFoundError, PermissionError, 
+            IsADirectoryError, NotADirectoryError, UnicodeEncodeError) as e:
+        print(f"Не удалось сохранить в {filepath}: {e}")
+    except (TypeError, ValueError, OverflowError) as err:
+        print(f"Не удалось записать данные в JSON для {filepath}: {err}")    
